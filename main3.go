ca457af49c4b0c8e20b11c22c93117d18b924351
phungvankhanh <vankhanhbk14@gmail.com>
2018-04-20 18:28:45 +0700

    add main3.go Chapter3

---
+++ main3.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("321325 x 424521=",321325 * 424521)
+		
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello " + "World")
+	
+}
